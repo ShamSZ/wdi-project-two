a7eddb5db2deea757b5565bb0293349874fc7761
ShamSZ <sham.szoda@gmail.com>
2018-10-27 16:11:00 +0100

    Created public folder and added style.css. Added layouts template and exported header/footer into partials. Created restaurants index page and route.

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const env = require('./config/environment');
+const expressLayouts = require('express-ejs-layouts');
+
 const app = express();
+
+const env = require('./config/environment');
 const restController = require('./controllers/restController');
+
+app.use(express.static('public'));
+app.use(expressLayouts);
 
 app.set('view engine', 'ejs');
 
 app.get('/', restController.home);
+app.get('/about', restController.about);
+app.get('/restaurants', restController.index);
 
-app.listen(env.port, ()=>console.log(`Up and running on port ${env.port}`));
+app.listen(env.port, () => console.log(`Up and running on port ${env.port}`));
--- controllers/restController.js
+++ controllers/restController.js
@@ -1,5 +1,9 @@
 const home = (req, res) => res.render('pages/home');
+const about = (req, res) => res.render('pages/about');
+const indexRest = (req, res) => res.render('restaurants/index');
 
 module.exports = {
-  home: home
+  home: home,
+  about: about,
+  index: indexRest
 };
