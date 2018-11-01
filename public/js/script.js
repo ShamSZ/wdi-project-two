2386874a79684cc570005b8f6438692c982067ba
ShamSZ <sham.szoda@gmail.com>
2018-11-01 23:56:34 +0000

    Changed website name and logo, changed stars to shurikens

--- public/js/script.js
+++ public/js/script.js
@@ -7,3 +7,19 @@ function navMenu(){
   });
 }
 navMenu();
+
+const allLinks = document.querySelectorAll('.nav-item');
+const allStars = document.querySelectorAll('label.rating-star');
+
+allStars.forEach(label => label.addEventListener('click', toggleActive));
+allLinks.forEach(link => link.addEventListener('click', toggleActiveLink));
+
+function toggleActive(e){
+  allStars.forEach(label => label.classList.remove('is-active'));
+  e.target.classList.add('is-active');
+}
+
+function toggleActiveLink(e){
+  allLinks.forEach(link => link.classList.remove('is-active'));
+  e.target.classList.add('is-active');
+}
