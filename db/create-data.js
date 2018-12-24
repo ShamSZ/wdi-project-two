2cdb2ae61b4f8ff6d5a0c23c1c97e30a20c2bed8
ShamSZ <sham.szoda@gmail.com>
2018-12-24 00:12:53 +0000

    Edited seeds

--- db/create-data.js
+++ db/create-data.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const env = require('../config/environment');
 const Restaurant = require('../models/restaurant');
 const User = require('../models/user');
-// Restaurant.collection.drop();
-// User.collection.drop();
+Restaurant.collection.drop();
+User.collection.drop();
 
 mongoose.connect(env.dbUri, { useNewUrlParser: true });
 
@@ -16,43 +16,37 @@ const userData = [
   }
 ];
 
-User.findByIdAndUpdate('5bdb96d8caeb3d0021486a6c', userData[0])
-  .then(user => {
-    console.log(user);
+User.create(userData).then((users) => {
+  console.log(`Created ${users.length} new users.`);
+  const restaurantsData = [
+    { name: 'Franco Manca',
+      price: 1,
+      description: 'Sourdough pizza',
+      cuisine: 'Pizza',
+      image: 'https://www.francomanca.co.uk/wp-content/uploads/2018/05/Ealing-external-1-4811-1000x723.jpg',
+      address: '10 Widmore Rd, Bromley BR1 1RY',
+      restAuthor: users[0]
+    },
+    { name: 'McDonalds',
+      price: 1,
+      description: 'Classic, long-running fast-food chain known for its burgers, fries & shakes.',
+      cuisine: 'Fast Food',
+      image: 'https://cdn.dnaindia.com/sites/default/files/styles/full/public/2018/04/03/667939-mcdonald-s-meal.png',
+      address: 'High St, Bromley BR1 1DN',
+      // restAuthor: '5bd6e6e147cd907e36fb0393'
+      restAuthor: users[0]
+    },
+    { name: 'Cafe Rouge',
+      price: 2,
+      description: 'Chain bistro for French classics from croque-monsieurs to mussels in a retro Parisian setting.',
+      cuisine: 'French',
+      image: 'https://cdn.prezzybox.com/Images/43526.jpg',
+      address: '12-13, Market Square, Bromley BR1 1NA',
+      restAuthor: users[0]
+    }
+  ];
+  Restaurant.create(restaurantsData).then((restaurants) => {
+    console.log(`Created ${restaurants.length} new restaurants.`);
     mongoose.connection.close();
   });
-
-// User.create(userData).then((users) => {
-//   console.log(`Created ${users.length} new users.`);
-//   const restaurantsData = [
-//     { name: 'Franco Manca',
-//       price: 1,
-//       description: 'Sourdough pizza',
-//       cuisine: 'Pizza',
-//       image: 'https://www.francomanca.co.uk/wp-content/uploads/2018/05/Ealing-external-1-4811-1000x723.jpg',
-//       address: '10 Widmore Rd, Bromley BR1 1RY',
-//       restAuthor: users[0]
-//     },
-//     { name: 'McDonalds',
-//       price: 1,
-//       description: 'Classic, long-running fast-food chain known for its burgers, fries & shakes.',
-//       cuisine: 'Fast Food',
-//       image: 'https://cdn.dnaindia.com/sites/default/files/styles/full/public/2018/04/03/667939-mcdonald-s-meal.png',
-//       address: 'High St, Bromley BR1 1DN',
-//       // restAuthor: '5bd6e6e147cd907e36fb0393'
-//       restAuthor: users[0]
-//     },
-//     { name: 'Cafe Rouge',
-//       price: 2,
-//       description: 'Chain bistro for French classics from croque-monsieurs to mussels in a retro Parisian setting.',
-//       cuisine: 'French',
-//       image: 'https://cdn.prezzybox.com/Images/43526.jpg',
-//       address: '12-13, Market Square, Bromley BR1 1NA',
-//       restAuthor: users[0]
-//     }
-//   ];
-//   Restaurant.create(restaurantsData).then((restaurants) => {
-//     console.log(`Created ${restaurants.length} new restaurants.`);
-//     mongoose.connection.close();
-//   });
-// });
+});
